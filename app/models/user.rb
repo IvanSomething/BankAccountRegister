eb838ff05efda3fd8c3100d52523e4cb7064c2a4
IvanSomething <ischlovski@gmail.com>
2021-08-15 11:04:34 +0200

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :first_name, :last_name, :patronimic, :identification_number, :tags, presence: true
+    validates :identification_number, uniqueness: true
 end
