8a2514ccdf6c1d524071bd06a07b65c18e9b9d43
IvanSomething <ischlovski@gmail.com>
2021-08-16 19:45:06 +0200

    Add validation to model Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,5 @@
 class Account < ApplicationRecord
   belongs_to :user
+  validates :currency, presence: true, uniqueness: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
