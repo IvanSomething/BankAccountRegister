7fbfe2d6471dc5d28ac195fa115818385ed23336
IvanSomething <ischlovski@gmail.com>
2021-08-16 19:24:33 +0200

    Add model Account

---
+++ db/migrate/20210816172254_create_accounts.rb
@@ -0,0 +1,11 @@
+class CreateAccounts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :accounts do |t|
+      t.string :currency
+      t.integer :amount, default: 0
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_095313) do
+ActiveRecord::Schema.define(version: 2021_08_16_172254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "currency"
+    t.integer "amount", default: 0
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_08_15_095313) do
     t.index ["identification_number"], name: "index_users_on_identification_number", unique: true
   end
 
+  add_foreign_key "accounts", "users"
 end
