c6fa7ab041c1ae76f0e04e392d2df27166a9df30
IvanSomething <ischlovski@gmail.com>
2021-08-28 14:08:28 +0200

    Update model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many  :accounts, dependent: :destroy
+    has_many :tags, dependent: :destroy
+
+    accepts_nested_attributes_for :tags
+
     validates :name, :surname, :patronymic_name, :identification_number, presence: true
     validates :identification_number, uniqueness: true
 end
