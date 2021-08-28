c581d96eb18aec7c9570181fb19edcab525bcf83
IvanSomething <ischlovski@gmail.com>
2021-08-28 12:14:23 +0200

    Add AccountsController

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,30 @@
+class AccountsController < ApplicationController
+    before_action :set_account, only: %i[destroy]
+
+    def create
+      user = User.find_by(identification_number: params[:identification_number])
+      
+      account = user.accounts.new(account_params)
+  
+      if account.save
+        render json: account, status: :created, location: account
+      else
+        render json: account.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @account.destroy
+    end
+  
+    private
+  
+    def set_account
+      @account = Account.find(params[:id])
+    end
+  
+    def account_params
+      params.require(:account).permit(:currency)
+    end
+  end
+end
