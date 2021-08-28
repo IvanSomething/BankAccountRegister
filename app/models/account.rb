5a4e33ff6e52ae8e7c53d4088fc79d0a1e13e4de
IvanSomething <ischlovski@gmail.com>
2021-08-28 14:20:00 +0200

    Add model Transaction

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,31 @@
 class Account < ApplicationRecord
   belongs_to :user
+
+  before_validation :set_amount
+
   validates :currency, presence: true
   validates :amount, numericality: { greater_than_or_equal_to: 0 }
   validate :currency_uniqueness
   
+  def to_deposit(amount)
+    self.amount += amount
+    true
+    save!
+  end
+
+  def withdraw(amount)
+    self.amount -= amount
+    true
+    save!
+  end
+
   private
 
   def currency_uniqueness
     errors.add(:currency, 'an account with this currency already exists') if user.accounts.find_by(currency: currency)
+  end
+
+  def set_amount
+    self.amount ||= 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_172254) do
+ActiveRecord::Schema.define(version: 2021_08_28_121742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,23 @@ ActiveRecord::Schema.define(version: 2021_08_16_172254) do
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "amout"
+    t.bigint "account_id", null: false
+    t.integer "deposit", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "surname"
@@ -35,4 +52,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_172254) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "tags", "users"
+  add_foreign_key "transactions", "accounts"
 end
