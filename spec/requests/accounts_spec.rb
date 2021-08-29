b970a072388ef3916b524f579e700bd18528293e
IvanSomething <ischlovski@gmail.com>
2021-08-29 17:41:18 +0200

    Add tests for transactions and accounts requests

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,7 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe "Accounts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:valid_attributes) do
+    {
+      currency: 'usd',
+      amount: 4
+    }
+  end
+
+  let(:user_attributes) do
+    {
+      name: 'name',
+      surname: 'surname',
+      patronymic_name: 'patronymic_name',
+      identification_number: 1,
+      tags_attributes: [{name: 'foo'}]
+    }
+  end
+
+  let(:user) do
+    User.create! user_attributes
+  end
+
+  let(:invalid_attributes) do
+    {
+      name: 'name',
+      name2: 'surname',
+      identification_number: 'foobar'
+    }
+  end
+
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      account = user.accounts.create valid_attributes
+      get account_url(account), as: :json
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Account' do
+        expect do
+          post accounts_url,
+               params: { account: valid_attributes, identification_number: user.identification_number }, as: :json
+        end.to change(Account, :count).by(1)
+      end
+
+      it 'renders a JSON response with the new user' do
+        post accounts_url,
+             params: { account: valid_attributes, identification_number: user.identification_number }, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including('application/json'))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Account' do
+        expect do
+          post accounts_url,
+               params: { account: invalid_attributes, identification_number: user.identification_number }, as: :json
+        end.to change(Account, :count).by(0)
+      end
+
+      it 'renders a JSON response with errors for the new user' do
+        post users_url,
+             params: { user: invalid_attributes, identification_number: user.identification_number }, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'destroys the requested Account' do
+      account = user.accounts.create valid_attributes
+      expect do
+        delete account_url(account), as: :json
+      end.to change(Account, :count).by(-1)
+    end
   end
 end
