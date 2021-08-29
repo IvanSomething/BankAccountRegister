b435995e7656686e36127d38874e47db7ed6a871
IvanSomething <ischlovski@gmail.com>
2021-08-29 17:25:56 +0200

    Add tests for User and Account models

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_attributes) do
+    {
+      name: 'name',
+      surname: 'surname',
+      patronymic_name: 'patronymic_name',
+      identification_number: 1,
+      tags_attributes: [{name: 'foo'}]
+    }
+  end
+
+  let(:account_attributes) do
+    {
+      currency: 'usd',
+      amount: '0'
+    }
+  end
+
+  let(:user) do
+    User.create! user_attributes
+  end
+
+  let(:account) do
+    user.accounts.create account_attributes
+  end
+
+  describe 'Validations' do
+    it 'is valid with the required attributes' do
+      expect(account).to be_valid
+    end
+
+    it 'is not valid without a currency' do
+      account.currency = nil
+      expect(account).not_to be_valid
+    end
+
+    it 'is not valid without a amount' do
+      account.amount = -1
+      expect(account).not_to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tags_attributes){[{name: 'foo'}]}
+  let(:user) do
+    described_class.new(
+      name: 'name',
+      surname: 'surname',
+      patronymic_name: 'patronymic_name',
+      identification_number: 1, 
+      tags_attributes: tags_attributes
+    )
+  end
+
+  describe 'Validations' do
+    it 'is valid with the required attributes' do
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      user.name = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a surname' do
+      user.surname = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without a patronymic_name' do
+      user.patronimic = nil
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid without an identification number' do
+      user.identification_number = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
