50329e7575290552b2b185a1be5e88f805f86965
IvanSomething <ischlovski@gmail.com>
2021-08-29 12:24:57 +0200

    Add controller Reports

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,23 @@
+class ReportsController < ApplicationController
+    def index
+        report, errors = ReportsManager.new(report_params).call
+    
+        if errors.blank?
+          render json: report, status: :created
+        else 
+          render json: errors, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def report_params
+        params.require(:account).permit(
+          :report_type,
+          :date_from,
+          :date_to,
+          user_ids:[],
+          tags:[],
+        )
+      end
+end
