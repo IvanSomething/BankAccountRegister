eb29f945837bf3b7d048ff0ee0facaf9ef5c93c3
IvanSomething <ischlovski@gmail.com>
2021-08-29 12:27:35 +0200

    Add routes for reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   resources :accounts, only: %i[create destroy]
-
   resources :transactions, param: :identification_number do
     post 'deposit', on: :member
     post 'transfer', on: :collection
   end
+  resources :reports, only: :index
 end
 
